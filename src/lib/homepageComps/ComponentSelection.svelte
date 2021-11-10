4f675e0c87ff3fd2c8e6f3fc47187ab21dfc7a78
grizhlieCodes <rafal.potasz@gmail.com>
2021-11-10 16:47:50 +0000

    height fix

--- src/lib/homepageComps/ComponentSelection.svelte
+++ src/lib/homepageComps/ComponentSelection.svelte
@@ -2,7 +2,7 @@
     export let showThisComponent, selectedComponent;
 </script>
 
-<div class="flex-grow flex flex-col h-[110vh] w-full items-center justify-center ">
+<div class="flex-grow flex flex-col min-h-[110vh] w-full items-center justify-center ">
 	{#if showThisComponent !== ''}
 		<svelte:component this={selectedComponent} />
 	{/if}
